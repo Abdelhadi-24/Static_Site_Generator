5120c2d0a708ffe4768b154258ec5d8bcb11ef6b
Abdelhadi-24 <kaddourabdelhadi24@gmail.com>
2026-09-24 16:40:37 +0100

    inline: extract links

--- src/test_inline_markdown.py
+++ src/test_inline_markdown.py
@@ -1,7 +1,7 @@
 import unittest
 
 from inline_markdown import (
-    split_nodes_delimiter,
+    split_nodes_delimiter, extract_markdown_images, extract_markdown_links
 )
 from textnode import TextNode, TextType
 
@@ -85,6 +85,80 @@ class TestInlineMarkdown(unittest.TestCase):
             ],
             new_nodes,
         )
+
+    # imgs + links
+
+    def test_extract_markdown_images(self):
+        matches = extract_markdown_images(
+            "This is text with an ![image](https://i.imgur.com/zjjcJKZ.png)"
+        )
+
+        self.assertListEqual(
+            [
+                ("image", "https://i.imgur.com/zjjcJKZ.png")
+            ],
+            matches,
+        )
+
+    def test_extract_multiple_images(self):
+        matches = extract_markdown_images(
+            "![cat](cat.png) and ![dog](dog.png)"
+        )
+
+        self.assertListEqual(
+            [
+                ("cat", "cat.png"),
+                ("dog", "dog.png"),
+            ],
+            matches,
+        )
+
+    def test_extract_markdown_links(self):
+        matches = extract_markdown_links(
+            "This is [Boot.dev](https://www.boot.dev)"
+        )
+
+        self.assertListEqual(
+            [
+                ("Boot.dev", "https://www.boot.dev")
+            ],
+            matches,
+        )
+
+    def test_extract_multiple_links(self):
+        matches = extract_markdown_links(
+            "[Boot.dev](https://www.boot.dev) and [YouTube](https://youtube.com)"
+        )
+
+        self.assertListEqual(
+            [
+                ("Boot.dev", "https://www.boot.dev"),
+                ("YouTube", "https://youtube.com"),
+            ],
+            matches,
+        )
+
+    def test_link_does_not_match_image(self):
+        matches = extract_markdown_links(
+            "![image](image.png)"
+        )
+
+        self.assertListEqual([], matches)
+
+    def test_no_images(self):
+        matches = extract_markdown_images(
+            "There are no images here."
+        )
+
+        self.assertListEqual([], matches)
+
+    def test_no_links(self):
+        matches = extract_markdown_links(
+            "There are no links here."
+        )
+
+        self.assertListEqual([], matches)
+
 
 
 if __name__ == "__main__":
